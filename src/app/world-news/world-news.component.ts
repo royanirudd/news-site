64a441834cb29f7d8ac22d61579b6c2d33bc6fec
royanirudd <aniruddroy704@gmail.com>
2024-10-07 11:12:30 +0000

    Added individual tab components

---
+++ src/app/world-news/world-news.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-world-news',
+  standalone: true,
+  imports: [],
+  templateUrl: './world-news.component.html',
+  styleUrl: './world-news.component.css'
+})
+export class WorldNewsComponent {
+
+}
