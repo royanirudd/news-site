edb45b24336ad5fc906d11fa43b00b0fcbd1ae14
royanirudd <aniruddroy704@gmail.com>
2024-10-07 11:40:46 +0000

    Added API calls

--- src/app/article-grid/article-grid.component.ts
+++ src/app/article-grid/article-grid.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewsService } from '../services/news.service';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-article-grid',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article-grid.component.html',
-  styleUrl: './article-grid.component.css'
+  styleUrls: ['./article-grid.component.css']
 })
-export class ArticleGridComponent {
+export class ArticleGridComponent implements OnInit {
+  @Input() category: string = '';
+  articles: Article[] = [];
 
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit() {
+    this.newsService.getTopHeadlines(this.category).subscribe(
+      (response: any) => {
+        this.articles = response.articles;
+      },
+      (error) => {
+        console.error('Error fetching news:', error);
+      }
+    );
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  newsApiKey: '5a98534ebfc24fe09730f5f88684aecf'
+};
